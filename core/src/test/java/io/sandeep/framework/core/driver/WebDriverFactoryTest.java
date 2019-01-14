8444dd1e59f5380c873213efec585d8d3bb7731a
Sandeep Singh <s.sandeep79@ymail.com>
2019-01-14 11:58:22 +0530

    tests for WebDriverFactory & various supported drivers written

---
+++ core/src/test/java/io/sandeep/framework/core/driver/WebDriverFactoryTest.java
@@ -0,0 +1,61 @@
+package io.sandeep.framework.core.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class WebDriverFactoryTest {
+    WebDriverFactory driverFactory;
+
+    @BeforeMethod
+    public void setUp () {
+        driverFactory = WebDriverFactory.getInstance();
+    }
+
+    @AfterMethod
+    public void tearDown () {
+        driverFactory = null;
+    }
+
+    @Test
+    public void testGetInstance () {
+        assertTrue(driverFactory instanceof WebDriverFactory);
+    }
+
+    @Test
+    public void testGetLocalChromeDriver () {
+        WebDriver local = driverFactory.getDriver("local");
+        assertTrue(local instanceof ChromeDriver);
+    }
+
+    // TODO: need to provide config params for RemoteDriver instance
+    @Test (enabled = false)
+    public void testGetRemoteChromeDriver () {
+        WebDriver remote = driverFactory.getDriver("remote");
+        assertTrue(remote instanceof RemoteDriver);
+    }
+
+    @Test (enabled = false)
+    public void testGetLocalFirefoxDriver () {
+        System.setProperty("browser", "firefox");
+        WebDriver local = driverFactory.getDriver("local");
+        assertTrue(local instanceof FirefoxDriver);
+    }
+
+    @Test
+    public void testCloseDriver () {
+        WebDriver local = driverFactory.getDriver("local");
+        local.quit(); local = null;
+        assertNull(local);
+    }
+
+    @Test(expectedExceptions = CloneNotSupportedException.class)
+    public void test_cannot_clone() throws CloneNotSupportedException {
+        driverFactory.clone();
+    }
+}
