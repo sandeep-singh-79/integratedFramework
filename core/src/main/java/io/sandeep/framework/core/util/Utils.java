5d3c7b0cfc7428ca8ee2060ee10e39a54aa03947
Sandeep Singh <s.sandeep79@ymail.com>
2019-01-14 17:28:14 +0530

    updated the timestamp to use '_' instead of ':'

--- core/src/main/java/io/sandeep/framework/core/util/Utils.java
+++ core/src/main/java/io/sandeep/framework/core/util/Utils.java
@@ -23,7 +23,7 @@ public class Utils {
     }
 
     public static String getTimeStamp () {
-        return (getTimeStamp("dd-MMM-yyyy HH:mm:ss").replaceAll(" ", ""));
+        return (getTimeStamp("dd-MMM-yyyy HH_mm_ss").replaceAll(" ", ""));
     }
 
     public static String getTimeStamp (String dateFormat) {
@@ -37,15 +37,16 @@ public class Utils {
     }
 
     public static File take_screenshot (WebDriver driver, final String method_name) {
-        if (driver == null) return null;
+        if (driver == null) throw new NullPointerException();
 
+        String screenshot_dir_path = new File(String.format("%s/../Screenshots/%s", System.getProperty("user.dir"), getDate())).getPath();
         File screenshotFile = null;
         try {
             TakesScreenshot screenshot = (TakesScreenshot) driver;
-            screenshotFile = create_screenshot_file(method_name,
-                    createDirectory(String.format("%s/Screenshots/%s", System.getProperty("user.dir"), getDate())));
+            screenshotFile = create_screenshot_file(method_name, createDirectory(screenshot_dir_path));
             copyFile(screenshot.getScreenshotAs(OutputType.FILE), screenshotFile);
         } catch (Exception ioe) {
+            log.error(screenshot_dir_path);
             log.error("Encountered issue while creating screenshot for method/scenario {}", method_name);
             log.error("This was caused by {}", ioe.getCause());
             log.error(Arrays.toString(ioe.getStackTrace()));
@@ -55,7 +56,7 @@ public class Utils {
     }
 
     private static File create_screenshot_file (final String method_name, final File screenshot_dir) {
-        return new File(String.format("%s/%s_%s.png", screenshot_dir.getPath(), method_name, Utils.getTimeStamp("dd-MM-yyyy_HH_mm_ss")));
+        return new File(String.format("%s/%s_%s.png", screenshot_dir.getAbsolutePath(), method_name, Utils.getTimeStamp("dd-MM-yyyy_HH_mm_ss")));
     }
 
     private static File createDirectory (String directoryPath) {
