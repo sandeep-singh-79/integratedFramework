0934065810bbe0e9235c318efd4da83a4ce39488
Sandeep Singh <s.sandeep79@ymail.com>
2019-01-14 10:58:42 +0530

    added tests for FrameworkConfig class

---
+++ core/src/test/java/io/sandeep/framework/core/config/FrameworkConfigTest.java
@@ -0,0 +1,37 @@
+package io.sandeep.framework.core.config;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Properties;
+
+import static org.testng.Assert.*;
+
+public class FrameworkConfigTest {
+    FrameworkConfig config;
+
+    @BeforeMethod
+    public void setUp () {
+        config = FrameworkConfig.getInstance();
+    }
+
+    @AfterMethod
+    public void tearDown () {
+    }
+
+    @Test
+    public void testGetInstance () {
+        assertTrue(config instanceof FrameworkConfig);
+    }
+
+    @Test
+    public void testGetConfigProperties () {
+        assertTrue(config.getConfigProperties() instanceof Properties);
+    }
+
+    @Test(expectedExceptions = CloneNotSupportedException.class)
+    public void test_cannot_clone() throws CloneNotSupportedException {
+        config.clone();
+    }
+}
