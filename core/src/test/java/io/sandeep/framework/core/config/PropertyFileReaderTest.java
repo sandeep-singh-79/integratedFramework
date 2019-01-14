e7f46d44ad4dd006139e2a2bb62aac69a8d0cc62
Sandeep Singh <s.sandeep79@ymail.com>
2019-01-14 10:49:58 +0530

    added test for reading a property file

--- core/src/test/java/io/sandeep/framework/core/config/PropertyFileReaderTest.java
+++ core/src/test/java/io/sandeep/framework/core/config/PropertyFileReaderTest.java
@@ -1,11 +1,17 @@
 package io.sandeep.framework.core.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Properties;
 
+import static org.testng.Assert.assertTrue;
+
+@Slf4j
 public class PropertyFileReaderTest {
     @BeforeMethod
     public void setUp () {
@@ -22,6 +28,13 @@ public class PropertyFileReaderTest {
 
     @Test (expectedExceptions = FileNotFoundException.class)
     public void testPropertyFileExists () throws FileNotFoundException {
-        new PropertyFileReader(new java.io.File(System.getProperty("user.dir") + "frameworkConfig.properties"));
+        new PropertyFileReader(new File(System.getProperty("user.dir") + "frameworkConfig.properties"));
+    }
+
+    @Test
+    public void testPropertyFilereturnsProperties() throws FileNotFoundException {
+        String filePath = String.format("%s/../testng/src/main/resources/frameworkConfig.properties", System.getProperty("user.dir"));
+        Properties props = new PropertyFileReader(new File(filePath)).getPropertyFile();
+        assertTrue(props instanceof Properties);
     }
 }
