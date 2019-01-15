a4b0cfcf4f4aa2ca56071dde76a2b6915470d722
Sandeep Singh <s.sandeep79@ymail.com>
2019-01-15 10:26:40 +0530

    using canonical path instead of Absolute path, providing format while creating timestamp

--- core/src/main/java/io/sandeep/framework/core/util/Utils.java
+++ core/src/main/java/io/sandeep/framework/core/util/Utils.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -35,23 +36,28 @@ public class Utils {
     public static File take_screenshot (WebDriver driver, final String method_name) {
         if (driver == null) throw new NullPointerException();
 
-        String screenshot_dir_path = new File(String.format("%s/../Screenshots/%s", System.getProperty("user.dir"), getDate())).getAbsolutePath();
+        String screenshot_dir_path = null;
+        try {
+            screenshot_dir_path = new File(String.format("%s/../Screenshots/%s", System.getProperty("user.dir"), getDate())).getCanonicalPath();
+        } catch (IOException e) {
+            log.error("error encountered while getting path of the screenshot directory!");
+        }
         File screenshotFile = null;
         try {
-            screenshotFile = create_screenshot_file(method_name, createDirectory(screenshot_dir_path));
-            copyFile(((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE), screenshotFile);
+            copyFile(((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE), create_screenshot_file(method_name,
+                    createDirectory(screenshot_dir_path)), true);
         } catch (Exception ioe) {
             log.error(screenshot_dir_path);
             log.error("Encountered issue while creating screenshot for method/scenario {}", method_name);
-            log.error("This was caused by {}", ioe.getCause());
+            log.error("This was caused by {}", ioe.getMessage());
             log.error(Arrays.toString(ioe.getStackTrace()));
         }
 
         return screenshotFile;
     }
 
-    private static File create_screenshot_file (final String method_name, final File screenshot_dir) {
-        return new File(String.format("%s/%s_%s.png", screenshot_dir.getAbsolutePath(), method_name, Utils.getTimeStamp()));
+    private static File create_screenshot_file (final String method_name, final File screenshot_dir) throws IOException {
+        return new File(String.format("%s/%s_%s.png", screenshot_dir.getCanonicalPath(), method_name, Utils.getTimeStamp("dd-MMM-yyyy_HH_mm_ss")));
     }
 
     private static File createDirectory (String directoryPath) {
